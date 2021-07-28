fb1da3e4013175004df39f4268dbc4eb9be98786
Виталя Викулин <livevikulin@gmail.com>
2021-07-28 14:46:46 +0700

    create object

---
+++ 10-Object/script.js
@@ -0,0 +1,36 @@
+// 1
+/*
+const users = [
+	{
+		username: 'David',
+		status: 'online',
+		lastActivity: 10
+	}, {
+ 
+		username: 'Lucy',
+		status: 'offline',
+		lastActivity: 22
+	}, {
+		username: 'Bob',
+		status: 'online',
+		lastActivity: 104
+	}
+]
+
+const onlineUsers = []
+let usersOnlineNames = []
+
+users.forEach( (item) => {
+	if ( item.status === 'online' ) {
+		onlineUsers.push(item)
+	}
+} )
+
+onlineUsers.forEach( (item) => {
+	usersOnlineNames.push(item.username)
+} )
+
+console.log(`Сейчас в онлайн следующие пользователи: ${usersOnlineNames}`)
+*/
+
+//2
